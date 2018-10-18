75ea741f015a6dbb8a565605c77783e74b53d296
Raghvendra Dixit <rdixit5@sapient.com>
2018-10-18 17:04:37 +0530

    reseting firebase file

--- server/firebasesetup.js
+++ server/firebasesetup.js
@@ -1,11 +1,6 @@
  const firebase=require('firebase');
    var config = {
-    apiKey: "AIzaSyBPIMdXPKEHfy_C8u19rLGqTDQjFJ-XHhU",
-    authDomain: "graphql-e4d94.firebaseapp.com",
-    databaseURL: "https://graphql-e4d94.firebaseio.com",
-    projectId: "graphql-e4d94",
-    storageBucket: "",
-    messagingSenderId: "457473126884"
+
   };
   firebase.initializeApp(config);
  const db=firebase.database();
